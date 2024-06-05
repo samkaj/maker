84b85ed64e07262724085f6538a95611fbfbd67b
samkaj <kajavasamuel@gmail.com>
2024-06-05 22:26:51 +0200

    [task] use clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
+use clap::Parser;
 use walker::Walker;
 
 pub mod walker;
 
-fn main() {
-    let root = String::from(".");
-    let ignore_dirs = vec![String::from("target"), String::from(".git")];
-    let walker = Walker::new(root, ignore_dirs);
+/// 🪄 Makefile generator
+#[derive(Parser)]
+struct Cli {
+    /// Source code root, usually `src`
+    path: String,
 
+    /// Output directory
+    #[arg(short, long, default_value_t = String::from("./target"))]
+    output_path: String,
+
+    /// Directories to ignore
+    #[arg(short, long)]
+    ignore_dirs: Vec<String>,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let walker = Walker::new(cli.path, cli.ignore_dirs);
     let files = walker.walk();
     for file in files {
         println!("{}", file);
